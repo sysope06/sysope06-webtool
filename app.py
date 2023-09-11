0e581f9db6a62f472bc953f771392606b6ade61d
T.Nakamura <sysope06@hotmail.com>
2023-09-11 11:01:03 +0900

    bug fix

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 import flask
 
-app = Flask(__name__, static_folder='.', static_url_path='')
+app = flask.Flask(__name__, static_folder='.', static_url_path='')
+
 @app.route('/')
 def index():
     return flask.__version__
+
+if __name__ == "__main__":
+    app.run(port=8000, debug=True)
